545d45e3ebd077a1fcc6388ea6249417e228f7ed
Giovanni Gomes <giovannigomes.snt@gmail.com>
2018-04-02 14:39:08 -0300

    menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import { MenudemoComponent } from './menudemo/menudemo.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {FormsModule} from '@angular/forms';
+import 'hammerjs';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {
+  MatAutocompleteModule, MatButtonToggleModule, MatCardModule, MatChipsModule,
+  MatDatepickerModule, MatDialogModule, MatDividerModule, MatExpansionModule,
+  MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule,
+  MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule,
+  MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule,
+  MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule,
+  MatTabsModule, MatToolbarModule, MatTooltipModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenudemoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule, FormsModule, MatButtonModule, MatCheckboxModule,
+    MatAutocompleteModule, MatButtonToggleModule, MatCardModule, MatChipsModule,
+    MatDatepickerModule, MatDialogModule, MatDividerModule, MatExpansionModule,
+    MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule,
+    MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule,
+    MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule,
+    MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule,
+    MatTabsModule, MatToolbarModule, MatTooltipModule
+  ],
+  exports: [
+    BrowserModule, BrowserAnimationsModule, FormsModule, MatButtonModule, MatCheckboxModule,
+    MatAutocompleteModule, MatButtonToggleModule, MatCardModule, MatChipsModule,
+    MatDatepickerModule, MatDialogModule, MatDividerModule, MatExpansionModule,
+    MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule,
+    MatNativeDateModule, MatPaginatorModule, MatProgressBarModule, MatProgressSpinnerModule,
+    MatRadioModule, MatRippleModule, MatSelectModule, MatSidenavModule, MatSliderModule,
+    MatSlideToggleModule, MatSnackBarModule, MatSortModule, MatStepperModule, MatTableModule,
+    MatTabsModule, MatToolbarModule, MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
